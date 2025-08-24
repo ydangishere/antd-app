a39cdfff755aa901b731787d5071e05d1deeaf9d
Dang Van Y <y.dangvan@gmail.com>
2025-08-25 00:44:39 +0700

    Keep only Ant Design SearchBox component and remove custom version

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import './App.css'
 import DiscardConfirmation from './components/DiscardConfirmation'
 import NewProfileForm from './components/NewProfileForm'
 import EmployeeTable from './components/EmployeeTable'
-import SearchBox from './components/SearchBox'
 import SearchBoxAntD from './components/SearchBoxAntD'
 
 function App() {
@@ -29,16 +28,11 @@ function App() {
       
       {showSearchBox && (
         <div style={{ maxWidth: '500px', margin: '20px auto', padding: '0 20px' }}>
-          <h3>Custom SearchBox Component</h3>
-          <SearchBox 
-            onSearch={(value) => console.log('Custom SearchBox:', value)}
-            onChange={(value) => console.log('Input changed:', value)}
-          />
-          
-          <h3 style={{ marginTop: '30px' }}>Ant Design SearchBox Component</h3>
+          <h3>Search Box</h3>
           <SearchBoxAntD 
-            onSearch={(value) => console.log('Ant Design SearchBox:', value)}
+            onSearch={(value) => console.log('Search:', value)}
             placeholder="Search by Name, ID or Email"
+            style={{ width: '100%' }}
           />
         </div>
       )}
